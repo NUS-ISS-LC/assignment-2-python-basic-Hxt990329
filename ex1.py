31f646ed0c8cbfe14608dcbdc7ea5d7b33abe09d
HU XINTIAN <hxt18647236209@gmail.com>
2025-11-09 12:19:00 +0000

    HU XINTIAN

--- ex1.py
+++ ex1.py
@@ -1,3 +1,9 @@
 def find(s, n):
 # write your implementation here
+lookup = {}
+    for i, num in enumerate(s):
+        complement = n - num
+        if complement in lookup:
+            return [lookup[complement], i]
+        lookup[num] = i
     return None
